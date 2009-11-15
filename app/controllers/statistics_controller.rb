e391770e7b8be93e563626d0a0c0ae1b55706ebd
Travis Roberts <traviskroberts@gmail.com>
2009-11-14 23:26:10 -0600

    Changed task display on statistics page to be grouped by task group

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,6 +2,7 @@ class StatisticsController < ApplicationController
   before_filter :require_user
   
   def index
+    @month_list = current_user.month_lists.first(:order => "year, month")
     @tasks = current_user.tasks.active
   end
   
