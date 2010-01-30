6cabe4c2d2e32784a3bed68e14ccb8f15073b81b
Travis Roberts <traviskroberts@gmail.com>
2010-01-30 15:09:26 -0600

    added total monthly spending graph to statistics page

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,13 +2,13 @@ class StatisticsController < ApplicationController
   before_filter :require_user
   
   def index
-    @month_list = current_user.month_lists.first(:order => "year, month")
+    @all_lists = current_user.month_lists.all(:order => 'year, month')
+    @month_list = @all_lists.first
     @tasks = current_user.tasks.active
   end
   
   def show
     @task = current_user.tasks.find(params[:id])
-    
   end
   
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,50 @@ module ApplicationHelper
     string.gsub(/\s+/,'-').gsub(/[^a-z0-9\-]+/i, '').gsub(/[\-]+/,'-').downcase
   end
   
+  def monthly_spending_graph(month_lists=[])
+    # don't bother if there isn't enough info for a graph
+    return "" if month_lists.blank?
+    
+    labels = []
+    amounts = []
+    
+    month_lists.each do |month_list|
+      labels << "#{month_list.month_name[0..2]}+#{month_list.year}"
+      amounts << month_list.lists.sum(:amount)
+    end
+    
+    # we need at least 12 values/labels for the graph to work
+    if amounts.length < 13
+      (12 - amounts.length).downto(1) do |x|
+        amounts.insert(0,0)
+        labels.insert(0,'-')
+      end
+    end
+    
+    # get the max amount for the range
+    max_amount = amounts.sort.last
+    
+    # add a little extra room at the top
+    max_amount += (max_amount / 10).ceil
+    
+    graph_url = "http://chart.apis.google.com/chart"                    # base graph url
+    graph_url << "?cht=bvs"                                             # set the chart type
+    graph_url << "&chs=790x350"                                         # set the chart size
+    graph_url << "&chf=bg,s,FFFFFF00"                                   # set the background color to transparent
+    graph_url << "&chco=1e9f24"                                         # set the color of the bars
+    graph_url << "&chtt=Total+Spending+(last+12+months)&chts=8e8e8a,28" # set the chart title
+    graph_url << "&chbh=39,25,25"                                       # set the bar width and spacing
+    graph_url << "&chxt=x,y"                                            # set the axes
+    graph_url << "&chds=0,#{max_amount}"
+    graph_url << "&chxl=0:|" + labels.join('|')                         # set the labels for the x-axis
+    graph_url << "&chxr=1,0,#{max_amount}"                              # set the labels for the y-axis
+    graph_url << "&chm=N*cUSD2*,555555,0,-1,12"                         # set the labels for each bar
+    graph_url << "&chg=10,10"                                           # set the grid lines
+    graph_url << "&chd=t:" + amounts.join(",")                          # add the data
+    
+    "<img src=\"#{graph_url}\" alt=\"overall graph\" />"
+  end
+  
   def draw_graph(task)
     lists = List.find(:all,
                       :include => :month_list,
