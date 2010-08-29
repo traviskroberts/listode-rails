5484cece9080232426b9d30c9f80bc15dafb6a64
Travis Roberts <traviskroberts@gmail.com>
2010-08-28 21:12:00 -0500

    Fixed ordering of statistics page for good.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,8 +2,8 @@ class StatisticsController < ApplicationController
   before_filter :require_user
   
   def index
-    @all_lists = current_user.month_lists.all(:order => 'year DESC, month DESC')
-    @month_list = @all_lists.first
+    @all_lists = current_user.month_lists.all(:order => 'year, month')
+    @month_list = @all_lists.last
     @tasks = current_user.tasks.active
   end
   
